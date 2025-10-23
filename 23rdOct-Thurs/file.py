08d9ad28b2ada41db0b13133757e5f111d7d4f3d
VikramVRDella <vikram@vrdella.com>
2025-10-23 10:08:29 +0530

    Signed-off-by: VikramVRDella <vikram@vrdella.com>

---
+++ 23rdOct-Thurs/file.py
@@ -0,0 +1,10 @@
+print(
+    '''
+    ***********************
+    *    File Handling    *
+    ***********************
+    '''
+)
+print("Opening a Text File...")
+f=open("demo.txt")
+print(f.read())
